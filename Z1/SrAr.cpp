7a99565c321f555214a36bcaed77651c59233913
Anasteisi <anasteisi91@yandex.ru>
2015-09-29 18:52:36 +0300

    Задача 01 с 26.09
    3 массива + среднее арифметическое каждого

---
+++ Z1/SrAr.cpp
@@ -0,0 +1,55 @@
+#include <ctime>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	srand(time(NULL));
+
+	{int n;
+	cout << "Введите количество элементов:" << endl;
+	cin >> n;//kolichextvo elementov v massive
+	int *x = new int[n];//massiv
+	double sum = 0;//summa elementov
+	for (int i = 0; i < n; ++i)
+	{
+		x[i] = rand() % 24;//[0;23]
+		cout << x[i] << endl;
+		sum = sum + x[i];
+	}
+	cout << "Среднее арифметическое:" << sum / n << endl;
+	delete[]x;}
+
+	{int n;
+	cout << "Введите количество элементов:" << endl;
+	cin >> n;//kolichextvo elementov v massive
+	int *x = new int[n];//massiv
+	double sum = 0;//summa elementov
+	for (int i = 0; i < n; ++i)
+	{
+		x[i] = 50 + rand() % 10;//[50;59]
+		cout << x[i] << endl;
+		sum = sum + x[i];
+	}
+	cout << "Среднее арифметическое:" << sum / n << endl;
+	delete[]x;}
+
+	{int n;
+	cout << "Введите количество элементов:" << endl;
+	cin >> n;//kolichextvo elementov v massive
+	int *x = new int[n];//massiv
+	double sum = 0;//summa elementov
+	for (int i = 0; i < n; ++i)
+	{
+		x[i] = rand() % 13 - 5;//[-7;5]
+		cout << x[i] << endl;
+		sum = sum + x[i];
+	}
+	cout << "Среднее арифметическое:" << sum / n << endl;
+	delete[]x;}
+
+	system("pause");
+	return 0;
+}
