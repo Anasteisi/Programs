39cf1e572303b01ae1b6355e3520e06448314b7e
Anasteisi <anasteisi91@yandex.ru>
2015-11-26 15:16:08 +0300

    Правка

--- F04/F04.cpp
+++ F04/F04.cpp
@@ -7,7 +7,7 @@ using namespace std;
 char *arab2rim(int arab) //funkcia dlya perevoda arabskogo chisla v rimskoe
 {
 	const int arabs[] = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };//massiv arabskih chisel
-	const char *rims[] = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };//massiv rimskih chisel
+	const char *rims[] = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };//massiv rimskih chisel, massiv strok = 2-hmerniy massiv simvolov
 
 	static char rim[20];//massiv dlya otveta
 
@@ -31,8 +31,8 @@ char *arab2rim(int arab) //funkcia dlya perevoda arabskogo chisla v rimskoe
 		if (arab >= arabs[n])//>=n-go elementa massiva arabs
 		{
 			rim[i++] = rims[n][0];// i-i element otveta rim = n-i element massiva rims
-			if (n & 1)
-				rim[i++] = rims[n][1];
+			if (n & 1)//chetnost' pozicii
+				rim[i++] = rims[n][1];//2 element rimzkoi cifry
 			arab -= arabs[n];//minus n-i element arabs
 		}
 		else
@@ -45,7 +45,7 @@ char *arab2rim(int arab) //funkcia dlya perevoda arabskogo chisla v rimskoe
 
 int main()
 {
-	char str[32];//stroka do ' '
+	char str[100];//stroka
 
 	ifstream in("in.txt");//fail
 	if (!in.is_open())
@@ -64,31 +64,36 @@ int main()
 		return 1;//zavershit' programmu
 	}
 
-	while (!in.eof())//poka ne konec in
+	while (!in.eof())//poka ne konec faila
 	{
-		in >> str;//stroka do ' '
-		if (strlen(str) == 10 && str[2] == '.' && str[5] == '.')//stroka iz 10 simvolov vida aa.bb.cccc
-		{
-			char den[3] = { str[0], str[1],'\0' };
-			char mes[3] = { str[3], str[4],'\0' };
-			char god[5] = { str[6], str[7], str[8], str[9], '\0' };
+		in.getline(str, 100);//stroka
+		char *context;//otslezhivayet, kakoe slovo chitaetsya, dlya povtornogo vyzova
+		char *sl = strtok_s(str, " ", &context);//slovo do " "
 
-			if (atoi(den) <= 31 && atoi(mes) <= 12)//31 den, 12 mes
+		while (sl != NULL)//poka ne konec stroki 
+		{
+			if (strlen(sl) == 10 && sl[2] == '.' && sl[5] == '.')//slovo iz 10 simvolov
 			{
-				char *rim;//god rimskimi ciframi
+				char den[3] = { sl[0], sl[1], '\0' };
+				char mes[3] = { sl[3], sl[4],'\0' };
+				char god[5] = { sl[6], sl[7], sl[8], sl[9], '\0' };
+
 				int arab = atoi(god);//god -> chislo
-				if (arab != 0)//chislo, ne 0
+
+				if (atoi(den) <= 31 && atoi(mes) <= 12 && arab != 0)//31 den, 12 mes, god -- chislo, ne 0
 				{
+					char *rim;//god rimskimi ciframi
 					rim = arab2rim(arab);//funkcia
-					out << den << "." << mes << "." << rim << endl;
+					out << den << "." << mes << "." << rim << " ";
 				}
-				else out << den << "." << mes << "." << god << endl;//bez izmenenii
+				else
+					out << sl << " ";//ne data, bez izmenenii
 			}
 			else
-				out << str << endl;//bez izmenenii
+				out << sl << " ";// ne 10, bez izmenenii
+			sl = strtok_s(NULL, " ", &context);//следующее слово
 		}
-		else
-			out << str << endl;//bez izmenenii
+		out << endl;//novaya stroka
 	}
 
 	out.close();//zakrytie faila
